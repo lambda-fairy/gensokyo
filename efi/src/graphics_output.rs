1e3848311379d495031245e4750981958dbbf1e5
Chris Wong <lambda.fairy@gmail.com>
2016-07-05 13:13:16 +1200

    Flesh out GOP wrapper even more!!!

--- efi/src/graphics_output.rs
+++ efi/src/graphics_output.rs
@@ -4,7 +4,7 @@ use core::ptr;
 use {EfiBox, EfiResult, Guid, PhysicalAddress, Protocol, check_status};
 use sys;
 
-pub use sys::{ModeInformation, PixelBitmask, PixelFormat};
+pub use sys::{BltPixel, ModeInformation, PixelBitmask, PixelFormat};
 
 pub struct GraphicsOutput(sys::GraphicsOutputProtocol);
 
@@ -13,23 +13,29 @@ impl Protocol for GraphicsOutput {
 }
 
 impl GraphicsOutput {
+    /// Returns the physical address and size of the linear frame buffer.
     pub fn linear_frame_buffer(&self) -> (PhysicalAddress, usize) {
         let mode = self.0.mode;
         unsafe { ((*mode).frame_buffer_base, (*mode).frame_buffer_size) }
     }
 
+    /// Returns the index of the current mode.
     pub fn current_mode(&self) -> ModeNumber {
         unsafe { (*self.0.mode).mode }
     }
 
+    /// Returns information pertaining to the current mode.
     pub fn current_mode_info(&self) -> &ModeInformation {
         unsafe { mem::transmute((*self.0.mode).info) }
     }
 
+    /// Returns the number of modes supported by this device. All mode numbers
+    /// are in the range `[0, max_mode)`.
     pub fn max_mode(&self) -> ModeNumber {
         unsafe { (*self.0.mode).max_mode }
     }
 
+    /// Queries information on the specified mode.
     pub fn query_mode(&self, mode: ModeNumber) -> EfiResult<EfiBox<ModeInformation>> {
         unsafe {
             let mut size_of_info: usize = 0;
@@ -44,11 +50,59 @@ impl GraphicsOutput {
         }
     }
 
+    /// Sets the mode of this device.
     pub fn set_mode(&self, mode: ModeNumber) -> EfiResult<()> {
         let status = unsafe {
             (self.0.set_mode)(
                 &self.0 as *const _ as *mut _,
                 mode)
+        };
+        check_status(status)
+    }
+
+    /// Fills the rectangle with a single color.
+    pub fn fill(
+        &self,
+        x: usize, y: usize,
+        w: usize, h: usize,
+        pixel: BltPixel) -> EfiResult<()>
+    {
+        let status = unsafe {
+            (self.0.blt)(
+                &self.0 as *const _ as *mut _,
+                &pixel as *const _ as *mut _,
+                sys::BltOperation::VideoFill,
+                0, 0,
+                x, y,
+                w, h,
+                0)
+        };
+        check_status(status)
+    }
+
+    /// Copies a pixel buffer to the screen.
+    pub fn copy_buffer_to_video(
+        &self,
+        sx: usize, sy: usize,
+        x: usize, y: usize,
+        w: usize, h: usize,
+        buffer: &[BltPixel],
+        row_len: usize) -> EfiResult<()> {
+        {
+            // Bounds check! ^_^
+            let row_len = if row_len == 0 { w } else { row_len };
+            assert!(buffer.len() >= (sy + h) * row_len);
+        }
+        let delta = row_len * mem::size_of::<BltPixel>();
+        let status = unsafe {
+            (self.0.blt)(
+                &self.0 as *const _ as *mut _,
+                buffer.as_ptr() as *mut _,
+                sys::BltOperation::BufferToVideo,
+                sx, sy,
+                x, y,
+                w, h,
+                delta)
         };
         check_status(status)
     }
